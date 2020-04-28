9f9dc6a25efcc5465b37e42903fe87c055798ddc
Tharindu <hourglass.monda@gmail.com>
2020-04-28 20:36:16 +0530

    pagination

--- src/components/Qcard.js
+++ src/components/Qcard.js
@@ -2,17 +2,123 @@ import React, { Component } from 'react';
 import Aux from './Wrap';
 import '../Style/Qcard.css';
 import areaImg from '../CoverImgs/_5area.jpg';
+import { Button } from 'react-bootstrap';
 
 class Qcard extends Component {
-    state = {  }
+    constructor(props){
+        super(props);
+        this.state = {
+            answers : this.props.Quations[0].otherAnswers,
+            UserAnswer: null
+        }
+    }
+
+   componentWillReceiveProps(nextProps){
+        this.refs.answer1.checked = false;
+        this.refs.answer2.checked = false;
+        this.refs.answer3.checked = false;
+        this.refs.answer4.checked = false;
+        this.suffleAnswer(nextProps.Quations[0].otherAnswers);
+        this.setState({
+            UserAnswer:null
+        })
+   }
+    suffleAnswer=(arry)=>{
+        let AnsArry = arry;
+        let i = AnsArry.length -1;
+        
+        for(i; i > 0 ; i--){
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = AnsArry[i];
+            AnsArry[i] =  AnsArry[j];
+            AnsArry[j] = temp;
+        }
+        
+        this.setState({
+            answers : AnsArry
+        })
+    }
+
+    submitAnswer=()=>{
+        if(this.state.UserAnswer === this.props.Quations[0].correctAnswer){
+            alert("Yes!! you are correct")
+        }
+        else alert("Ohh!!! You are wrong")
+    }
+    ckeckBoxHandlerOne=()=>{
+        this.refs.answer2.checked = false;
+        this.refs.answer3.checked = false;
+        this.refs.answer4.checked = false;
+        if(this.refs.answer1.checked){
+            this.setState({
+                UserAnswer:this.refs.answer1.value
+            })
+        }
+        else
+        {
+            this.setState({
+                UserAnswer:null
+            })
+        }
+    }
+    ckeckBoxHandlerTwo=()=>{
+        this.refs.answer1.checked = false;
+        this.refs.answer3.checked = false;
+        this.refs.answer4.checked = false;
+        if(this.refs.answer2.checked){
+            this.setState({
+                UserAnswer:this.refs.answer2.value
+            })
+        }
+        else
+        {
+            this.setState({
+                UserAnswer:null
+            })
+        }
+    }
+    ckeckBoxHandlerThree=()=>{
+        this.refs.answer1.checked = false;
+        this.refs.answer2.checked = false;
+        this.refs.answer4.checked = false;
+        if(this.refs.answer3.checked){
+            this.setState({
+                UserAnswer:this.refs.answer3.value
+            })
+        }
+        else
+        {
+            this.setState({
+                UserAnswer:null
+            })
+        }
+    }
+    ckeckBoxHandlerFour=()=>{
+        this.refs.answer1.checked = false;
+        this.refs.answer2.checked = false;
+        this.refs.answer3.checked = false;
+        if(this.refs.answer4.checked){
+            this.setState({
+                UserAnswer:this.refs.answer4.value
+            })
+        }
+        else
+        {
+            this.setState({
+                UserAnswer:null
+            })
+        }
+    }
+   
     render() { 
         return ( 
             <Aux>
                 <div className="row">
-                    <div className="col-2"></div>
+                    <div className="col-2">
+                    </div>
                     <div className="Qcard col-8">
                         <h1 className="quation">
-                            <span>01 </span>පහත දී ඇති රුපයේ පරිමිතිය සොයන්න 
+                            <span>01 </span>{this.props.Quations[0].quation}
                         </h1>
                         <div className="qImg">
                             <img src={areaImg} className="quationImg" alt="quationimage" />
@@ -21,37 +127,55 @@ class Qcard extends Component {
                             <div className="col-6">
                                 <div className="answer">
                                     <label className="lebAns">
-                                        <input type="checkbox" value=""/>
-                                        &nbsp;&nbsp;26 cm
+                                        <input type="checkbox" 
+                                        value={this.state.answers[0]}
+                                        ref="answer1"
+                                        onClick={this.ckeckBoxHandlerOne}
+                                        />
+                                        &nbsp;&nbsp;{this.state.answers[0]}
                                     </label>
                                 </div>
                                 <div className="answer">
                                     <label className="lebAns">
-                                        <input type="checkbox" value=""/>
-                                        &nbsp;&nbsp;23 cm
+                                        <input type="checkbox" 
+                                        value={this.state.answers[1]}
+                                        ref="answer2"
+                                        onClick={this.ckeckBoxHandlerTwo}
+                                        />
+                                        &nbsp;&nbsp;{this.state.answers[1]}
                                     </label>
                                 </div>
                             </div>
                             <div className="col-6">
                                 <div className="answer">
                                     <label className="lebAns">
-                                        <input type="checkbox" value=""/>
-                                        &nbsp;&nbsp;24 cm
+                                        <input type="checkbox" 
+                                        value={this.state.answers[2]}
+                                        ref="answer3"
+                                        onClick={this.ckeckBoxHandlerThree}
+                                        />
+                                        &nbsp;&nbsp;{this.state.answers[2]}
                                     </label>
                                 </div>
                                 <div className="answer">
                                     <label className="lebAns">
-                                        <input type="checkbox" value=""/>
-                                        &nbsp;&nbsp;25 cm
+                                        <input type="checkbox"
+                                        value={this.state.answers[3]}
+                                        ref="answer4"
+                                        onClick={this.ckeckBoxHandlerFour}
+                                        />
+                                        &nbsp;&nbsp;{this.state.answers[3]}
                                     </label>
                                 </div>
                             </div>
                         </div>
-                        <div>
-                            <a href="#/" className="btn btn-dark submitBtn mb-3">Submit</a>
+                        <div className="btnWrap">
+                            <button className="submitBtn mb-3" onClick={this.submitAnswer}>Submit</button>
                         </div>
                     </div>
-                    <div className="col-2"></div>
+                    <div className="col-2">
+                        <Button onClick={this.props.paginateAnswer}>Next</Button>
+                    </div>
                 </div>
                
             </Aux>
--- src/pages/Quationing.js
+++ src/pages/Quationing.js
@@ -3,13 +3,50 @@ import Layout from '../components/Layout';
 import Qcard from '../components/Qcard';
 import '../Style/common.css'
 class Examing extends Component {
-    state = {  }
+    state = { quations:[
+                    {   
+                        "quationId":1,
+                        "quation":"පහත දී ඇති රුපයේ පරිමිතිය සොයන්න",
+                        "correctAnswer":"24 cm",
+                        otherAnswers:["25 cm","23 cm","22 cm","24 cm"]
+                    },
+                    {
+                        "quationId":2,
+                        "quation":"පැතත්ක  දිග 4cm  සමචතුරශ්‍රයක පරිමිතිය සොයන්න ",
+                        "correctAnswer":"16 cm",
+                        otherAnswers:["20 cm","15 cm","17 cm","16 cm"]
+                    },
+                    {   
+                        "quationId":3,
+                        "quation":"පැතත්ක  දිග 9 cm  සවිධි පන්චාශ්‍රයක  පරිමිතිය සොයන්න ",
+                        "correctAnswer":"45 cm",
+                        otherAnswers:["42 cm","50 cm","48 cm","45 cm"]
+                    }
+                ] ,
+                currentPage:1,
+                quationPerPage:1
+            }   
+        
+            paginate=()=>{
+                let i = this.state.currentPage;
+                i++;
+                if(i<=3){
+                    this.setState({currentPage:i})
+                }  
+            }
+
     render() { 
+        const indexOfLastQuation = this.state.currentPage * this.state.quationPerPage;
+        const indexOfFirstQuation = indexOfLastQuation - this.state.quationPerPage;
+        const currentQuation = this.state.quations.slice(indexOfFirstQuation,indexOfLastQuation);
         return (
                 <Layout>
                     <div>
                         <h6 className="root">Grade 5 / ganithaya / Parimithiya</h6>
-                        <Qcard/>
+                        
+                            <Qcard Quations={currentQuation} 
+                            paginateAnswer={this.paginate}/>
+
                     </div>
                 </Layout>
            );
